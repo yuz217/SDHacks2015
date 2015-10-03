09293f0e98c7d1b5b94e57d7dc8af60f056ff7cd
jteo1 <jteo1@yahoo.com>
2015-10-03 16:16:08 -0700

    trying to fix database query

--- app.js
+++ app.js
@@ -19,8 +19,9 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
+app.use(express.bodyParser);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
