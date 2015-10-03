747fbdeb1b1227f25795bbe1debb4a9ff85a4144
Kevin Zhu <kevinazhu@gmail.com>
2015-10-03 08:10:57 -0700

    began implementation of voting. added jquery bower

--- public/javascripts/socketio.js
+++ public/javascripts/socketio.js
@@ -5,6 +5,8 @@ myApp.controller('socketCtrl', function($scope, $sce) {
     $scope.messagesListSafe = "";
 
     var messagesList = [];
+    var votesList = [];
+    var count = 0;
 
     $scope.sendMessage = function() {
         //console.log($scope.message);
@@ -12,9 +14,25 @@ myApp.controller('socketCtrl', function($scope, $sce) {
     };
 
     socket.on('emit message', function(msg){
+        messagesList.push(msg);
         $scope.$apply(function() {
-            messagesList.push("<li>" + msg + "</li>");
-            $scope.messagesListSafe = $sce.trustAsHtml(messagesList.join(''))
+            postMessages();
         });
     });
+
+    $scope.end = function() {
+        messagesList.splice(1, messagesList.length);
+        postMessages();
+    };
+
+    function postMessages() {
+        var messagesHtmlList = [];
+        for(var i = 0; i < messagesList.length; i++) {
+            votesList.push("vote" + count);
+            var voteNum = "vote" + i;
+            messagesHtmlList.push("<li><button id='" + voteNum + "'>Vote" + i + "</button></li>");
+        }
+        $scope.messagesListSafe = $sce.trustAsHtml(messagesHtmlList.join(''));
+        console.log(votesList[0]);
+    }
 });
