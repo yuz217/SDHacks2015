aec1d4c23eb02ee9815e2b1fc383578a40176c7c
Taylor Shin <t2shin@ucsd.edu>
2015-10-03 15:20:37 -0700

    fixing modal.

--- public/javascripts/modal-username.js
+++ public/javascripts/modal-username.js
@@ -3,8 +3,13 @@ $(document).ready(function() {
   //var from = $("#username").val();
   //console.log(from);
   $('#submit').on('click', function(e) {
-    var from = $("#username").val();
-    console.log(from);
+    var username = $("#username").val();
+    // Set cookie to username in order to keep track of anonymous name
+    document.cookie = username;
+    console.log(document.cookie);
+    //document.getElementById('nameofuser').textContent = document.cookie;
+    
+    // Hide the dialog pop up after entering name
     $('#modal-username').modal('hide');
   })
 });
