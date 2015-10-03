107c3c35a4cc94022d215b5e9cbde7c392ef9566
Taylor Shin <t2shin@ucsd.edu>
2015-10-03 08:10:16 -0700

    Got username modal to save name locally.

--- public/javascripts/modal-username.js
+++ public/javascripts/modal-username.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
   $('#modal-username').modal(options);
-  var from = $("#username").val();
-  console.log(from);
+  //var from = $("#username").val();
+  //console.log(from);
+  $('#submit').on('click', function(e) {
+    var from = $("#username").val();
+    console.log(from);
+    $('#modal-username').modal('hide');
+  })
 });
 
 var options = {
