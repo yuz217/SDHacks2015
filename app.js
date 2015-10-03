ebfc61c2dc2d2beae611737659ca46e5eebbc4d1
jteo1 <jteo1@yahoo.com>
2015-10-03 16:37:09 -0700

    revert

--- app.js
+++ app.js
@@ -19,7 +19,6 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(express.bodyParser);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
