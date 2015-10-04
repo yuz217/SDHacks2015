c63ea88728ab73a430687c9d9e9ad5c678e8c762
jteo1 <jteo1@yahoo.com>
2015-10-03 23:27:48 -0700

    using escaped parameter inputs for sentence add

--- routes/addSentence.js
+++ routes/addSentence.js
@@ -13,8 +13,8 @@ var router = express.Router();
 
       connection.connect();
 
-      var input = '(NULL' + ',' + req.query.storyID + ',' + req.query.sentence + ',' + req.query.author + ')';
-      connection.query('INSERT INTO sentences (sentenceID, storyID, sentence, author) VALUES ?', input, function(err, rows, fields) {
+      var input = [req.query.storyID, req.query.sentence, req.query.author];
+      connection.query('INSERT INTO sentences (sentenceID, storyID, sentence, author) VALUES (NULL, ?, ?, ?);', input, function(err, rows, fields) {
         if (!err)
         {
           console.log('The solution is: ', rows);
@@ -30,7 +30,7 @@ var router = express.Router();
       connection.end();
 }
 
-router.get('/', function(req, res) {
+router.post('/', function(req, res) {
     addSentence(req,res);
 });
 
