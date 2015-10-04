a7064ea0a68686b5cbfec9c7a3cccb7889b205a9
Kevin Zhu <kevinazhu@gmail.com>
2015-10-04 05:18:03 -0700

    fixed timer issues completely? need to add database

--- public/javascripts/write-story.js
+++ public/javascripts/write-story.js
@@ -7,6 +7,10 @@ myApp.controller('StoryCtrl', function($scope, $sce) {
     var votesList;
     var messageCounter = 0;
 
+    var begin = false;
+
+    $('#inputArea').prop('disabled', true);
+
     $scope.sendMessage = function() {
         socket.emit('chat message', $scope.message);
         $scope.message = "";
@@ -14,47 +18,61 @@ myApp.controller('StoryCtrl', function($scope, $sce) {
     };
 
     $scope.vote = function(index) {
-        $('#vote' + index).prop('disabled', true);
-        $('#vote' + index).removeClass("btn-default");
-        $('#vote' + index).addClass("btn-success");
-        socket.emit('vote', index);
+        if(begin) {
+            $('#vote' + index).prop('disabled', true);
+            $('#vote' + index).removeClass("btn-default");
+            $('#vote' + index).addClass("btn-success");
+            socket.emit('vote', index);
+        }
     };
 
     socket.on('emit message', function(msg){
-        $scope.$apply(function() {
-            $scope.messageList.push({ msg: msg, votes: 0 });
-        });
+        if(begin) {
+            $scope.$apply(function() {
+                $scope.messageList.push({ msg: msg, votes: 0 });
+            });
+        }
     });
 
     socket.on('emit start vote', function() {
-        $scope.$apply(function() {
-            $('#inputArea').prop('disabled', true);
-            votesList = [];
-            $("#messageList div button").css("visibility", "visible");
-            for(var i = 0; i < $scope.messageList.length - messageCounter; i++) {
-                votesList[i] = 0;
-            }
-        });
-        socket.emit('end vote timer');
+        if(begin) {
+            $scope.$apply(function() {
+                $('#inputArea').prop('disabled', true);
+                votesList = [];
+                $("#messageList div button").css("visibility", "visible");
+                for(var i = 0; i < $scope.messageList.length - messageCounter; i++) {
+                    votesList[i] = 0;
+                }
+            });
+            socket.emit('end vote timer');
+        }
     });
 
     socket.on('emit vote', function(index){
-        $scope.$apply(function() {
-            var adjustedIndex = index - messageCounter
-            votesList[adjustedIndex] += 1;
-            $scope.messageList[index].votes = votesList[adjustedIndex];
-        });
+        if(begin) {
+            $scope.$apply(function() {
+                var adjustedIndex = index - messageCounter
+                votesList[adjustedIndex] += 1;
+                $scope.messageList[index].votes = votesList[adjustedIndex];
+            });
+        }
     });
 
     socket.on('emit end vote', function() {
         $scope.$apply(function() {
-            $("#messageList div button").remove();
-            var maxVotesIndex = votesList.indexOf(Math.max.apply(Math, votesList));
-            $scope.messageList.splice(messageCounter + maxVotesIndex + 1, $scope.messageList.length - (maxVotesIndex+1));
-            $scope.messageList.splice(messageCounter, maxVotesIndex);
-            messageCounter++;
+            if(begin) {
+                $("#messageList div button").remove();
+                var maxVotesIndex = votesList.indexOf(Math.max.apply(Math, votesList));
+                $scope.messageList.splice(messageCounter + maxVotesIndex + 1, $scope.messageList.length - (maxVotesIndex+1));
+                $scope.messageList.splice(messageCounter, maxVotesIndex);
+                messageCounter++;
+            }
+            else {
+                // CALL DATABASE HERE and then start
+            }
             $('#inputArea').prop('disabled', false);
         });
         socket.emit('start vote timer');
+        begin = true;
     });
 });
